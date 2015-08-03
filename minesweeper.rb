ed84b69b25e703ab882b5352cb45f0f867b0adaf
Ingrid & Oscar <Ingrid+Oscar@appacademy.io>
2015-08-03 13:34:58 -0400

    initialized mine_positions as an instance variable.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,23 +1,25 @@
 class Minesweeper
   NUM_BOMBS = 10
   BOARD_SIZE = 9
-  attr_reader :board
+  attr_reader :board, :mine_positions
 
   def initialize
     @board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) }
+    @mine_positions = add_mines
   end
 
   def populate_board
-    mine_positions = add_mines
     board.each_index do |row|
       board.each_index do |col|
         bombed = false
         bombed = true if mine_positions.include?([row, col])
-        board[row][col] = Tile.new(bombed)
+        board[row][col] = Tile.new(bombed,neighbor_bombs)
       end
     end
 
   end
+
+
 
   def add_mines
     mine_positions = []
@@ -41,11 +43,12 @@ class Minesweeper
 end
 
 class Tile
-  attr_accessor :bombed, :flagged, :revealed
-  def initialize(bombed)
+  attr_accessor :bombed, :flagged, :revealed, :neighbor_bombs
+  def initialize(bombed,neighbor_bombs)
     @bombed = bombed
     @flagged = false
     @revealed = false
+    @neighbor_bombs = neighbor_bombs
   end
 
   def to_s
