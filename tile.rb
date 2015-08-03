c1cb115a86cb3fda4e9208e6da62677a99b74502
Ingrid & Oscar <Ingrid+Oscar@appacademy.io>
2015-08-03 17:44:59 -0400

    Added colors table to tile class

--- tile.rb
+++ tile.rb
@@ -1,6 +1,19 @@
+require 'colorize'
+
 class Tile
   attr_accessor :flagged, :revealed
   attr_reader :num_bombs, :has_bomb
+
+  NUM_COLORS = {
+    1 => :blue,
+    2 => :green,
+    3 => :red,
+    4 => :dark_blue,
+    5 => :brown,
+    6 => :teal,
+    7 => :black,
+    8 => :grey
+  }
 
   def initialize(has_bomb, num_bombs)
     @has_bomb = has_bomb
