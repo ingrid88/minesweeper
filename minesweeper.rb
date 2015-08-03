47fa073fda6fa312936edda75959e02433ce656a
Ingrid & Oscar <Ingrid+Oscar@appacademy.io>
2015-08-03 12:14:13 -0400

    built populate board method to fill board with empty tiles.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,21 +1,26 @@
 class Minesweeper
   NUM_BOMBS = 10
   BOARD_SIZE = 9
+  attr_reader :board
 
   def initialize
     @board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) }
   end
 
   def populate_board
-    
+    board.each_index do |row|
+      board.each_index do |col|
+        board[row][col] = Tile.new
+      end
+    end
 
   end
 end
 
 class Tile
   attr_accessor :bombed, :flagged, :revealed
-  def initialize(bombed)
-    @bombed = bombed
+  def initialize
+    @bombed = false
     @flagged = false
     @revealed = false
   end
